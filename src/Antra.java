2653a7559c1829f566a4303fe85ffe14860b1329
vilmamac <mackeviciene.vilma@gmail.com>
2018-11-05 19:41:13 +0200

    a

---
+++ src/Antra.java
@@ -0,0 +1,9 @@
+
+public class Antra {
+
+	public static void main(String[] args) {
+		System.out.println("Cia jau antra klase Git'e");
+
+	}
+
+}
---
+++ src/Pirma.java
@@ -0,0 +1,8 @@
+
+public class Pirma {
+
+	public static void main(String[] args) {
+		System.out.println("Pirma klase Git'e ! Truputi pakeiciau");
+	}
+
+}
